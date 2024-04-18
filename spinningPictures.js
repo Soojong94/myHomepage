50f98f88d2f437a9ab8a29952d786b80ef6b3e92
Soojong Kim <sujjong456@gmail.com>
2024-04-18 11:18:12 +0900

    0418 홈페이지

--- spinningPictures.js
+++ spinningPictures.js
@@ -1,23 +1,26 @@
-var container = document.querySelector('.container')
-      var overlay = document.querySelector('.overlay')
-      container.addEventListener('mousemove', function (e) {
-        var x = e.offsetX
-        var y = e.offsetY
-        var rotateY = -1 / 5 * x + 20
-        var rotateX = 4 / 15 * y - 30
+function applyRotation(container, overlay) {
+  container.addEventListener('mousemove', function (e) {
+    var x = e.offsetX;
+    var y = e.offsetY;
+    var rotateY = -1 / 5 * x + 20;
+    var rotateX = 4 / 15 * y - 30;
 
-        
-  rotateY = (rotateY / 250) * 90; // 
-  rotateX = (rotateX / 250) * 90; // 
-        
-        console.log('mousemove', x,y)
+    rotateY = ((rotateY / 250) * 90 ) % 360;
+    rotateX = ((rotateX / 250) * 90 ) % 360;
 
-        overlay.style = `background-position : ${x / 5 + y / 5}%; filter : opacity(${x / 200}) brightness(1.2)`
+    overlay.style = `background-position : ${x / 5 + y / 5}%; filter : opacity(${x / 200}) brightness(1.2)`;
+    container.style = `transform : perspective(350px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  });
 
-        container.style = `transform : perspective(350px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
-      })
-      container.addEventListener('mouseout', function () {
-        overlay.style = 'filter : opacity(0)'
-        container.style = 'transform : perspective(350px) rotateY(0deg) rotateX(0deg)'
-      })
+  container.addEventListener('mouseout', function () {
+    overlay.style = 'filter : opacity(0)';
+    container.style = 'transform : perspective(350px) rotateY(0deg) rotateX(0deg)';
+  });
+}
 
+// 각각의 카드에 함수를 적용
+var containers = document.querySelectorAll('.container');
+containers.forEach(function (container) {
+  var overlay = container.querySelector('.overlay');
+  applyRotation(container, overlay);
+});
