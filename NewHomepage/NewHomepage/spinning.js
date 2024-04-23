e9cca3fce9fced78f65db00fa5ebf7e6a5495f5f
Soojong Kim <sujjong456@gmail.com>
2024-04-23 18:03:29 +0900

    0423

--- NewHomepage/NewHomepage/spinning.js
+++ NewHomepage/NewHomepage/spinning.js
@@ -2,9 +2,9 @@ function applyRotation(image) {
   image.addEventListener('mousemove', function (e) {
     var x = e.offsetX;
     var y = e.offsetY;
-    
-    var speed = 0.3; 
-    
+
+    var speed = 0.3;
+
     var rotateY = -1 / 5 * x + 20;
     var rotateX = 4 / 15 * y - 30;
 
@@ -12,10 +12,10 @@ function applyRotation(image) {
     rotateX = ((rotateX / 250) * 90 + 360) % 360;
 
     // 각도를 높이기 위해 추가한 변수
-    var angleMultiplier = 1; 
+    var angleMultiplier = 1.5;
 
     rotateY = (rotateY + x * speed * angleMultiplier) % 360;
-    rotateX = (rotateX + y * speed * angleMultiplier) % 360; 
+    rotateX = (rotateX + y * speed * angleMultiplier) % 360;
 
     image.style.transform = `perspective(350px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
   });
@@ -26,7 +26,7 @@ function applyRotation(image) {
 }
 
 var image5 = document.querySelector('.image5');
-applyRotation(image5); 
+applyRotation(image5);
 
 var image7 = document.querySelector('.image7');
 applyRotation(image7); 
